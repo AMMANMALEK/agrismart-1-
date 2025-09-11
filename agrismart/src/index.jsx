da95573fcb189b475e5464cb774dae85d0668bd7
sarhan31 <sarhanvohra31@gmail.com>
2025-09-12 01:02:23 +0530

    s15

--- agrismart/src/index.jsx
+++ agrismart/src/index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/tailwind.css";
 import "./styles/index.css";
-import "./i18n";
+import "./i18n/i18n";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
